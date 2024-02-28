2db6e1ee7b26eea3a207ffb3cadef61a12f9f258
amcphillips <a.j.mcphillips@gmail.com>
2024-02-28 22:56:31 +0000

    general updates

--- foreign_ownership.R
+++ foreign_ownership.R
@@ -9,7 +9,7 @@ library(sf)
 
 # Import raw data
 
-raw_data <- read_csv("E:/R data/OCOD_FULL_2024_02.csv")
+raw_data <- read_csv("E:/R data/Foreign property/OCOD_FULL_2024_02.csv")
 
 # Clean column titles
 raw_data <- raw_data |>
@@ -20,11 +20,20 @@ core <- raw_data |>
   select(title_number, property_address, district, region, postcode, price_paid, country_incorporated_1)
  
 # Create geographic summary  
-geog_sum <- core |>
-  group_by(region, district) |> 
-  summarise(n = sum(n()))
+region <- core |>
+  group_by(region) |> 
+  summarise(n = sum(n())) |>
+  mutate(prop = scales::label_percent()(n / sum(n)))
 
+LA <- core |>
+  group_by(district) |> 
+  summarise(n = sum(n())) |>
+  mutate(prop = scales::label_percent()(n / sum(n)))
 
+source <- core |>
+  group_by(country_incorporated_1) |>
+  summarise(n = sum(n())) |>
+  mutate(prop = scales::label_percent()(n / sum(n)))
 
 # Only include those with a price paid entry which should only be residential properties
 price <- core |>
@@ -36,9 +45,10 @@ price_sum <- price |>
   summarise_each(funs(sum, max, min, mean, median), price_paid)
   #summarise(n = sum(n()))
   
-write.csv(geog_sum, file = "E:/R data/geog_sum.csv",row.names=FALSE)
-write.csv(price_sum, file = "E:/R data/price_sum.csv",row.names=FALSE) 
-
+write.csv(region, file = "E:/R data/Foreign property/region.csv",row.names=FALSE)
+write.csv(LA, file = "E:/R data/Foreign property/LA.csv",row.names=FALSE)
+write.csv(price_sum, file = "E:/R data/Foreign property/price_sum.csv",row.names=FALSE) 
+write.csv(source, file = "E:/R data/Foreign property/source.csv",row.names=FALSE) 
 
 
 
